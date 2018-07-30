9800bf0bb4a6c0ac1de334cef96c4d760c69f3c3
ganature <498857839@qq.com>
2018-07-30 18:31:35 +0800

    1

--- static/js/project.js
+++ static/js/project.js
@@ -4,5 +4,10 @@ function show_validate_msg(elem,status,msg){
 	if(status=='success'){
 		$(elem).parent().addClass("has-success");
 		$(elem).next('span').text(msg);
+	}else if(status=='erroe'){
+		$(elem).parent().addClass('has-error');
+		$(elem).next('span').text(msg);
+	})
+	
 	}
-}
+function 
