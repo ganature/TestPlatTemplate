c4a3248f26a14fc9d510ee68bd3d718d76d83224
ganature <498857839@qq.com>
2018-07-30 18:25:05 +0800

    1

---
+++ static/js/project.js
@@ -0,0 +1,8 @@
+function show_validate_msg(elem,status,msg){
+	$(elem).parent().removeClass("has-success has-error");
+	$(elem).next('span').text("");
+	if(status=='success'){
+		$(elem).parent().addClass("has-success");
+		$(elem).next('span').text(msg);
+	}
+}
