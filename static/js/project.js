e1999dbc60d148126aab79a21de55e7e1b4982a1
ganature <498857839@qq.com>
2018-07-31 17:54:26 +0800

    123

--- static/js/project.js
+++ static/js/project.js
@@ -10,4 +10,47 @@ function show_validate_msg(elem,status,msg){
 	})
 	
 	}
-function 
+$(function () {
+        $('form').bootstrapValidator({
+
+　　　　　　　　message: 'This value is not valid',
+            　feedbackIcons: {
+                　　　　　　　　valid: 'glyphicon glyphicon-ok',
+                　　　　　　　　invalid: 'glyphicon glyphicon-remove',
+                　　　　　　　　validating: 'glyphicon glyphicon-refresh'
+            　　　　　　　　   },
+            fields: {
+                project_name: {
+                    message: '项目名称验证失败',
+                    validators: {
+                        notEmpty: {
+                            message: '项目名称不能为空'
+                        },
+						stringLength: {
+                            min: 6,
+                            max: 18,
+                            message: '项目名称长度必须在6到18位之间'
+                        },
+                        regexp: {
+                            regexp: /^[a-zA-Z0-9_]+$/,
+                            message: '项目名称只能包含大写、小写、数字和下划线'
+                        }
+                    }
+                },
+                project_detail: {
+                    validators: {
+                        notEmpty: {
+                            message: '邮箱地址不能为空'
+                        }
+                    }
+                },
+				project_remark: {
+					validators: {
+						notEmpty: {
+							message: '邮箱地址不能为空'
+						}
+					}
+				}
+            }
+        });
+    });
